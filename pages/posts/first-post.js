2a834893711c14d6504ee8f43ea6fe285d257d36
achrysaetos <achrysaetos@yahoo.com>
2022-05-21 16:47:45 -0500

    Add pic and tab title

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,13 @@
+import Head from 'next/head';
 import Link from 'next/link';
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+        {/* Option to load third-party scripts here too */}
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
