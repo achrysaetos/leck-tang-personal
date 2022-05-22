40095168ed167e0a1526c91fa5749bf3e4d4b99d
achrysaetos <achrysaetos@yahoo.com>
2022-05-22 12:50:46 -0500

    Format date using date-fns

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,8 @@
+import Head from 'next/head';
 import Layout from '../../components/layout';
+import Date from '../../components/date';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import utilStyles from '../../styles/utils.module.css';
 
 // Fetch necessary data to render the post with the given id
 export async function getStaticProps({ params }) {
@@ -23,13 +26,17 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      {/* Third-party scripts can go here too */}
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
