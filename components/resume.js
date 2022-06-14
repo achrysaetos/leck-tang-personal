87a5ce52d818b32957dea80a23c6cf5c6417acf5
Leck Tang <achrysaetos@yahoo.com>
2022-06-14 17:32:49 -0500

    Add resume

--- components/resume.js
+++ components/resume.js
@@ -1,4 +1,6 @@
 import utilStyles from "../styles/utils.module.css";
+import { useState } from "react";
+import cx from "classnames";
 
 const classes = {
   cs: [
@@ -17,15 +19,108 @@ const classes = {
     "Financial Markets",
     "Econometrics",
   ],
-  activities: [
-    "Yale Computer Society",
-    "Yale Entrepreneurial",
-  ]
+  activities: ["Yale Computer Society", "Yale Entrepreneurial Society"],
 };
 
+function CoursePicker() {
+  const categories = ["cs", "econ", "activities"];
+  const [selected, setSelected] = useState({
+    cs: false,
+    econ: false,
+    activities: false,
+  });
+  const { cs, econ, activities } = classes;
+
+  return (
+    <div className="border rounded-xl p-2 px-4 my-4">
+      <div className="flex">
+        <div className="font-bold mr-auto">Courses</div>
+        <div className="flex gap-2">
+          {categories.map((c) => (
+            <button
+              className={cx(
+                "rounded px-3 text-gray-500",
+                selected[c] && " bg-green-200 text-black"
+              )}
+              onClick={() => setSelected({ ...selected, [c]: !selected[c] })}
+            >
+              {c}
+            </button>
+          ))}
+        </div>
+      </div>
+      {selected.cs && (
+        <div className="mt-3">
+          <div className="text-gray-400 uppercase text-sm">CS</div>
+          {cs.join(", ")}
+        </div>
+      )}
+      {selected.econ && (
+        <div className="mt-3">
+          <div className="text-gray-400 uppercase text-sm">econ</div>
+          {econ.join(", ")}
+        </div>
+      )}
+      {selected.activities && (
+        <div className="mt-3">
+          <div className="text-gray-400 uppercase text-sm">activities</div>
+          {activities.join(", ")}
+        </div>
+      )}
+    </div>
+  );
+}
+
+function SkillsPicker() {
+  const categories = ["cs", "econ", "activities"];
+  const [selected, setSelected] = useState({
+    cs: false,
+    econ: false,
+    activities: false,
+  });
+  const { cs, econ, activities } = classes;
+
+  return (
+    <div className="border rounded-xl p-2 px-4 my-4">
+      <div className="flex">
+        <div className="font-bold mr-auto">Skills</div>
+        <div className="flex gap-2">
+          {categories.map((c) => (
+            <button
+              className={cx(
+                "rounded px-3 text-gray-500",
+                selected[c] && " bg-green-200 text-black"
+              )}
+              onClick={() => setSelected({ ...selected, [c]: !selected[c] })}
+            >
+              {c}
+            </button>
+          ))}
+        </div>
+      </div>
+      {selected.cs && (
+        <div className="mt-3">
+          <div className="text-gray-400 uppercase text-sm">CS</div>
+          {cs.join(", ")}
+        </div>
+      )}
+      {selected.econ && (
+        <div className="mt-3">
+          <div className="text-gray-400 uppercase text-sm">econ</div>
+          {econ.join(", ")}
+        </div>
+      )}
+      {selected.activities && (
+        <div className="mt-3">
+          <div className="text-gray-400 uppercase text-sm">activities</div>
+          {activities.join(", ")}
+        </div>
+      )}
+    </div>
+  );
+}
 
 export default function Resume() {
-  const { cs, econ, activities } = classes;
   return (
     <>
       <h2 className={utilStyles.headingLg}>Education</h2>
@@ -41,19 +136,9 @@ export default function Resume() {
           <p className="text-gray-400 text-sm">May 2023</p>
         </div>
       </div>
-      <details className="mt-4">
-        <div className="flex flex-wrap pl-4">{cs.join(", ")}</div>
-        <summary className="select-none cursor-pointer">Relevant CS Courses</summary>
-      </details>
-      <details className="mt-1">
-        <div className="flex flex-wrap pl-4">{econ.join(", ")}</div>
-        <summary className="select-none cursor-pointer">Relevant ECON Courses</summary>
-      </details>
-      <details className="mt-1">
-        <div className="flex flex-wrap pl-4">{activities.join(", ")}</div>
-        <summary className="select-none cursor-pointer">Activities and Societies</summary>
-      </details>
-      <h2 className={utilStyles.headingLg}>Skills</h2>
+      <CoursePicker />
+      <SkillsPicker />
+
       <h2 className={utilStyles.headingLg}>Experience</h2>
       <h2 className={utilStyles.headingLg}>Projects</h2>
     </>
